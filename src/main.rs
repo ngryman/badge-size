382677a01d2b8f917720470aee22b8a30b2ab977
Nicolas Gryman <ngryman@gmail.com>
2021-11-09 12:04:42 +0100

    fix: append to log file instead of truncating it

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,14 @@ async fn init_logger(logpath: Option<String>) -> Result<()> {
   let mut logger = Logger::try_with_env_or_str("info,surf=error,tide=error")?;
 
   logger = if let Some(logpath) = logpath {
-    logger.log_to_file(
-      FileSpec::default()
-        .directory(logpath)
-        .basename("badgesize")
-        .use_timestamp(false),
-    )
+    logger
+      .log_to_file(
+        FileSpec::default()
+          .directory(logpath)
+          .basename("badgesize")
+          .use_timestamp(false),
+      )
+      .append()
   } else {
     logger.log_to_stdout()
   };
